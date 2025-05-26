7f99ee1fe4634033ac955404b8e3f2d69a01638c
Mohammed Bakkali <94966939+mohammed-bakkali@users.noreply.github.com>
2025-05-26 14:56:07 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-EMAIL_ADDRESS = "mohammedbakkali807@gmail.com"
-EMAIL_PASSWORD = "gjvx lwxg lsrc lslm"
+EMAIL_ADDRESS = "xxxxxxx@gmail.com"
+EMAIL_PASSWORD = "xxxxxxxxxxxxxxx"
 SEND_INTERVAL = 60  # in seconds
 LOG_FILE_PATH = "logs/log.txt"
